a11ce1327236fc15ee24d1d7a4c9a0e203e1f470
Slav <shutrow@rambler.ru>
2022-01-19 12:18:45 +0300

    Parsing HTTP Methods

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ var server = http.createServer(function(req, res) {
   var path = parsedUrl.pathname
   var trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
+  var method = req.method.toLowerCase()
+
   res.end('Hello World!\n')
 
-  console.log('Request received  on path: ' + trimmedPath)
+  console.log(
+    'Request received  on path: ' + trimmedPath + 'with method ' + method
+  )
 })
 
 server.listen(3000, function() {
