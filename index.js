114b38eb9b78bf03ce5462a0d954e853fb1f0742
Slav <shutrow@rambler.ru>
2022-01-19 05:07:59 +0300

    Parsing request Path

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var http = require('http')
+var url = require('url')
 
 var server = http.createServer(function(req, res) {
+  var parsedUrl = url.parse(req.url, true)
+
+  var path = parsedUrl.pathname
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
   res.end('Hello World!\n')
+
+  console.log('Request received  on path: ' + trimmedPath)
 })
 
 server.listen(3000, function() {
