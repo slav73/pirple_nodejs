38d529227ced24bf8de176a83b99fecc5d3eb4e1
Slav <shutrow@rambler.ru>
2022-01-20 12:45:31 +0300

    Adding config.js with NODE_ENV variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http')
 var url = require('url')
 var StringDecoder = require('string_decoder').StringDecoder
+var config = require('./config')
 
 var server = http.createServer(function(req, res) {
   var parsedUrl = url.parse(req.url, true)
@@ -50,8 +51,14 @@ var server = http.createServer(function(req, res) {
   })
 })
 
-server.listen(3000, function() {
-  console.log('The server is listening on port 3000 now')
+server.listen(config.port, function() {
+  console.log(
+    'The server is listening on port ' +
+      config.port +
+      ' in ' +
+      config.envName +
+      ' mode'
+  )
 })
 
 var handlers = {}
