14591f0cc03b68cf734bdecc3d648e4f4c2ca9c0
Slav <shutrow@rambler.ru>
2022-01-26 01:30:32 +0300

    background workers done

--- index.js
+++ index.js
@@ -1,87 +1,14 @@
-var http = require('http')
-var https = require('https')
-var url = require('url')
-var StringDecoder = require('string_decoder').StringDecoder
-var config = require('./lib/config')
-var fs = require('fs')
-var handlers = require('./lib/handlers')
-var helpers = require('./lib/helpers')
+var server = require('./lib/server')
+var workers = require('./lib/workers')
 
-helpers.sendTwilioSms('4158375309', 'Hello', function(err) {
-  console.log('this was an error ', err)
-})
+var app = {}
 
-var httpServer = http.createServer(function(req, res) {
-  unifiedServer(req, res)
-})
+app.init = function() {
+  server.init()
 
-httpServer.listen(config.httpPort, function() {
-  console.log('The server is listening on port ' + config.httpPort)
-})
-
-var httpsServerOptions = {
-  key: fs.readFileSync('./https/key.pem'),
-  cert: fs.readFileSync('./https/cert.pem'),
-}
-var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
-  unifiedServer(req, res)
-})
-
-httpsServer.listen(config.httpsPort, function() {
-  console.log('The server is listening on port ' + config.httpsPort)
-})
-
-var unifiedServer = function(req, res) {
-  var parsedUrl = url.parse(req.url, true)
-
-  var path = parsedUrl.pathname
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '')
-
-  var queryStringObject = parsedUrl.query
-
-  var headers = req.headers
-
-  var method = req.method.toLowerCase()
-
-  var decoder = new StringDecoder('utf-8')
-  var buffer = ''
-  req.on('data', function(data) {
-    buffer += decoder.write(data)
-  })
-
-  req.on('end', function() {
-    buffer += decoder.end()
-
-    var chosenHandler =
-      typeof router[trimmedPath] !== 'undefined'
-        ? router[trimmedPath]
-        : handlers.notFound
-
-    var data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: helpers.parseJsonToObject(buffer),
-    }
-
-    chosenHandler(data, function(statusCode, payload) {
-      statusCode = typeof statusCode === 'number' ? statusCode : 200
-      payload = typeof payload === 'object' ? payload : {}
-
-      var payloadString = JSON.stringify(payload)
-
-      res.setHeader('Content-Type', 'application/json')
-      res.writeHead(statusCode)
-      res.end(payloadString)
-      console.log('Returning: ', statusCode, payloadString)
-    })
-  })
+  workers.init()
 }
 
-var router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens,
-  checks: handlers.checks,
-}
+app.init()
+
+module.exports = app
